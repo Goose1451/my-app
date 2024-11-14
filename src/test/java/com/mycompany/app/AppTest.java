5aad62175c39c2c3956ae77e4aec456cb510ef7f
Wes <wgodard@gmail.com>
2024-11-13 21:29:08 -0800

    Issues to demo developer workflow Fixes #13

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -5,12 +5,13 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Unit test for simple App. TODO: #12 create issues
  */
 public class AppTest 
 {
     /**
      * Rigorous Test :-)
+     * I've resolved #13
      */
     @Test
     public void shouldAnswerWithTrue()
